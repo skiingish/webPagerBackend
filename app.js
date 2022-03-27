6866a17f67b57459ab23dd969193708de932750b
Sean <52271972+skiingish@users.noreply.github.com>
2022-03-27 19:05:37 +1100

    change up for deployment

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
+var logger = require('morgan');
 
 const app = express();
 
 // Connect Database
 connectDB();
 
+// Don't show the log in test.
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+
 // Init middleware
 app.use(express.json({ extended: false }));
+
+//To allow cross-origin requests
+app.use(cors());
 
 // Default Route.
 app.get('/', (req, res) => res.send('API'));
@@ -17,7 +27,5 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/order', require('./routes/api/order'));
 
-// Open Port
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export app
+module.exports = app;
